e8d45223f66ab01385c06266ba152820ba8eae43
=AnhHoangNgo <=ngoanh1292000@gmail.com>
2019-09-25 18:26:06 +0700

    commit

---
+++ js/controller.js
@@ -0,0 +1,50 @@
+const controller ={};
+controller.loatScript = function(src){
+	return new Promise((resolve, reject)=>{
+	const script = document.createElement('script');
+	script.src=src;
+	document.head.append(script);
+	script.onload=()=>{
+		resolve();
+	};
+	script.onerror=()=>{
+		throw new Error('error loading...!');
+	}
+})
+};
+controller.loadCss = function(href){
+	return new Promise((resolve, reject)=>{
+		const link = document.createElement('link');
+		link.rel = "stylesheet";
+		link.href=href;
+		document.head.append(link);
+		link.onload=()=>{
+			resolve();
+		}
+		link.onerror=()=>{
+			throw new Error ('error loading...!');
+		}
+	})
+};
+controller.loadAPI = function(url){
+	fetch(url).then(
+		(res)=>{
+			return res.json();
+		}
+	)
+}
+
+ controller.setPage = function (){
+	 const queryObjects={};
+	 const queryStringPage = location.search;
+	 const query = queryStringPage.replace('?','').replace(/-/gi,' ').split('&')
+	 console.log(query);
+	 query.forEach((element)=>{
+		 console.log(element);
+		const x =  element.split('=');
+			 queryObjects[x[0]]= x[1]
+	 })
+	 return queryObjects;
+	};
+
+	
